ba14a520674441ca47f2e4fa8853b227fb81a255
Srinu <csrinu236@gmail.com>
2021-08-29 16:34:22 +0530

    DONE..

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,23 @@
 const numbers = [...document.querySelectorAll(".timeline-center .number")];
 const years = [...document.querySelectorAll(".timeline-center h4")];
+const navbar = document.querySelector(".navbar");
+const sideBar = document.querySelector(".side-bar");
+const closeBtn = document.querySelector(".close-btn");
+const navBtn = document.querySelector(".nav-btn");
 const date = document.getElementById("date");
 
 const year = new Date().getFullYear();
 date.textContent = year;
 numbers.reverse();
+
+window.addEventListener("scroll", function () {
+  const height = this.window.pageYOffset;
+  if (height > 72) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+});
 
 window.addEventListener("load", function () {
   numbers.forEach((item, index) => {
@@ -13,4 +26,11 @@ window.addEventListener("load", function () {
   years.forEach((item, index) => {
     item.textContent = year - index;
   });
+});
+
+closeBtn.addEventListener("click", function () {
+  sideBar.classList.remove("show-sidebar");
+});
+navBtn.addEventListener("click", function () {
+  sideBar.classList.add("show-sidebar");
 });
