0387133292a36782da2b51cd186601d2d1f430d4
Srinu <csrinu236@gmail.com>
2021-08-29 13:21:28 +0530

    All done

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,11 @@
 const numbers = [...document.querySelectorAll(".timeline-center .number")];
 const years = [...document.querySelectorAll(".timeline-center h4")];
-const year = new Date().getFullYear();
+const date = document.getElementById("date");
 
+const year = new Date().getFullYear();
+date.textContent = year;
 numbers.reverse();
+
 window.addEventListener("load", function () {
   numbers.forEach((item, index) => {
     item.textContent = index + 1;
